fcec42af8f81ccc8821df7200f172e911001b060
Leonan Marques <leonan.marques886@gmail.com>
2023-12-02 13:57:13 -0300

    for loop

---
+++ src/nativeMain/kotlin/Condicional_For.kt
@@ -0,0 +1,26 @@
+fun main() {
+    for (i in 1..10) {
+        println(i)
+    }
+
+    for(i in 1..< 10) {
+        println(i)
+    }
+
+    for(i in 10 downTo 1) {
+        println(i)
+    }
+
+    for(i in 1..10 step 2) {
+        println(i)
+    }
+
+    for(i in 1 until 10) {
+        println(i)
+    }
+
+    val str = "Hello"
+    for (c in str) {
+        println(c)
+    }
+}
