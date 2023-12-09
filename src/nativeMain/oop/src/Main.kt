c39b3b4593b01f307eb6b09bc6aedbebe4a6a625
Leonan Marques <leonan.marques886@gmail.com>
2023-12-09 19:43:20 -0300

    new constructors have been implemented

--- src/nativeMain/oop/src/Main.kt
+++ src/nativeMain/oop/src/Main.kt
@@ -6,4 +6,9 @@ fun main() {
     println(pessoa.saudacao())
     pessoa.aniversario()
     println(pessoa.saudacao())
+
+    val pessoa2 = Pessoa("Mary", 30, "Feminino")
+    println(pessoa2.saudacao())
+    pessoa2.aniversario()
+    println(pessoa2.saudacao())
 }
--- src/nativeMain/oop/src/Pessoa.kt
+++ src/nativeMain/oop/src/Pessoa.kt
@@ -1,9 +1,15 @@
 class Pessoa(val nome: String, var idade: Int) {
+    var sexo: String? = null
+
+    constructor(nome: String, idade: Int, sexo: String) : this(nome, idade) {
+        println("Construtor secundário")
+        this.sexo = sexo
+    }
     fun aniversario() {
         idade++
     }
 
     fun saudacao() {
-        println("Olá, meu nome é $nome e tenho $idade anos")
+        println("Olá, meu nome é $nome, sou do sexo $sexo e tenho $idade anos")
     }
 }
